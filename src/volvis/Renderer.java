91d909a754d2f3653efe740cfdb7fd1d55b8a917
surajiyer96 <suraj.iyer96@outlook.com>
2016-11-25 00:17:50 +0100

    MIP + Trilinear interpolation implemented

---
+++ src/volvis/Renderer.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volvis;
+
+import com.jogamp.opengl.GL2;
+import java.util.ArrayList;
+import util.TFChangeListener;
+
+/**
+ *
+ * @author michel
+ */
+public abstract class Renderer {
+     int winWidth, winHeight;
+    boolean visible = false;
+    boolean interactiveMode = false;
+    ArrayList<TFChangeListener> listeners = new ArrayList<TFChangeListener>();
+
+    public Renderer() {
+        
+    }
+
+    public void setInteractiveMode(boolean flag) {
+        interactiveMode = flag;
+    }
+    
+    public void setWinWidth(int w) {
+        winWidth = w;
+    }
+
+    public void setWinHeight(int h) {
+        winHeight = h;
+    }
+
+    public int getWinWidth() {
+        return winWidth;
+    }
+
+    public int getWinHeight() {
+        return winHeight;
+    }
+
+    public void setVisible(boolean flag) {
+        visible = flag;
+    }
+
+    public boolean getVisible() {
+        return visible;
+    }
+
+    public void addTFChangeListener(TFChangeListener l) {
+        if (!listeners.contains(l)) {
+            listeners.add(l);
+        }
+    }
+    
+    public abstract void visualize(GL2 gl);
+}
